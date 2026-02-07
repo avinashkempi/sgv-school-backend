1ef9bc64f1fd314f1271914b9bd17a717e6e02b3
avinashkempi <avinash.kempi@gmail.com>
2026-02-07 10:46:38 +0530

    Fix: Add missing backend dependencies and cleanup routes

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ app.use('/api/dashboard', require('./src/routes/dashboardRoutes'));
 app.use('/api/search', require('./src/routes/search'));
 app.use('/api/attendance-enhancements', require('./src/routes/attendanceEnhancements'));
 app.use('/api/fee-enhancements', require('./src/routes/feeEnhancements'));
-app.use('/api/fee-enhancements', require('./src/routes/feeEnhancements'));
 app.use('/api/analytics', require('./src/routes/analytics'));
 app.use('/api/import', require('./src/routes/import'));
 
